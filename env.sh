969bf794c639c9f4ae53a76f5f42e26bdcd0b19e
Dumlu Timuralp <34840364+dumlutimuralp@users.noreply.github.com>
2023-11-21 22:37:04 +0000

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,13 +1,18 @@
 export AccountId=$(aws sts get-caller-identity --query "Account" --output text)
 
-export EnvironmentName=$(aws cloudformation describe-stacks --stack-name awsblogstack --query 'Stacks[0].Parameters[?ParameterKey == `EnvironmentName`].ParameterValue' --output text)
+export EnvironmentName=$(aws cloudformation describe-stacks --stack-name awsblogstack --query "Stacks[0].Parameters[?ParameterKey == 'EnvironmentName'].ParameterValue" --output text)
 
-export AWS_REGION=$(aws ec2 describe-availability-zones --output text --query 'AvailabilityZones[0].[RegionName]')
+export AWS_REGION=$(aws ec2 describe-availability-zones --output text --query "AvailabilityZones[0].[RegionName]")
 
-export PrivateSubnet1=$(aws ec2 describe-subnets --filters "Name=tag:aws:cloudformation:logical-id,Values=PrivateSubnet1" --query "Subnets[*].SubnetId" --output text)
+export TargetGroup1ARN=$(aws cloudformation describe-stacks --stack-name awsblogstack --query "Stacks[0].Outputs[?contains(OutputKey, 'TargetGroup1ARN')].OutputValue" --output text)
 
-export PrivateSubnet2=$(aws ec2 describe-subnets --filters "Name=tag:aws:cloudformation:logical-id,Values=PrivateSubnet2" --query "Subnets[*].SubnetId" --output text)
+export TargetGroup2ARN=$(aws cloudformation describe-stacks --stack-name awsblogstack --query "Stacks[0].Outputs[?contains(OutputKey, 'TargetGroup2ARN')].OutputValue" --output text)
+
+export PrivateSubnet1=$(aws cloudformation describe-stacks --stack-name awsblogstack --query "Stacks[0].Outputs[?contains(OutputKey, 'PrivateSubnet1')].OutputValue" --output text)
+
+export PrivateSubnet2=$(aws cloudformation describe-stacks --stack-name awsblogstack --query "Stacks[0].Outputs[?contains(OutputKey, 'PrivateSubnet2')].OutputValue" --output text)
 
 export Cluster1Name=cluster1
 
 export Cluster2Name=cluster2
+
