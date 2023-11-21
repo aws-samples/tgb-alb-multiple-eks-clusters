e8175a7bad2a9396ad94ecf99e57c3e7598742c2
Dumlu Timuralp <34840364+dumlutimuralp@users.noreply.github.com>
2023-11-21 20:19:12 +0000

    Add files via upload

---
+++ aws_load_balancer_controller.sh
@@ -0,0 +1,11 @@
+eksctl utils associate-iam-oidc-provider --region ${AWS_REGION} --cluster ${Cluster1Name} --approve
+
+curl -o iam-policy.json curl -O https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.6.0/docs/install/iam_policy.json
+
+aws iam create-policy --policy-name AWSLoadBalancerControllerIAMPolicy --policy-document file://iam_policy.json
+
+eksctl create iamserviceaccount --cluster=${Cluster1Name} --namespace=kube-system --name=aws-load-balancer-controller --attach-policy-arn=arn:aws:iam::${AccountId}:policy/AWSLoadBalancerControllerIAMPolicy --override-existing-serviceaccounts --region ${AWS_REGION} --approve
+
+helm repo add eks https://aws.github.io/eks-charts
+
+helm install aws-load-balancer-controller eks/aws-load-balancer-controller -n kube-system --set clusterName=${Cluster1Name} --set serviceAccount.create=false --set serviceAccount.name=aws-load-balancer-controller
